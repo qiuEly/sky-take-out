3af7325cc10d56042b2ecd6638ac159588a91280
bronya <2354796263@qq.com>
2023-10-26 22:21:09 +0800

    忽略掉dev.yml

---
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -0,0 +1,32 @@
+package com.sky.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈〉
+ *
+ * @author ely
+ * @create 2023/10/26
+ * @since 1.0.0
+ */
+//TODO redis配置类
+@Configuration
+@Slf4j
+public class RedisConfiguration {
+   @Bean
+   public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory){
+       log.info("开始创建redisTemplate");
+       RedisTemplate redisTemplate = new RedisTemplate();
+       //工厂类
+       redisTemplate.setConnectionFactory(redisConnectionFactory);
+       //key序列化
+       redisTemplate.setKeySerializer(redisTemplate.getStringSerializer());
+       return redisTemplate;
+   }
+}
