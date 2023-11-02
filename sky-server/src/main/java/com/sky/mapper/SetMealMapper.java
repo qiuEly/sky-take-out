19b77e39906a3fc5b6e14fa73a90d95b48dfd9f4
bronya <2354796263@qq.com>
2023-11-02 20:04:14 +0800

    day12

--- sky-server/src/main/java/com/sky/mapper/SetMealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetMealMapper.java
@@ -13,6 +13,7 @@ import org.apache.ibatis.annotations.Select;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface SetMealMapper {
@@ -63,4 +64,6 @@ public interface SetMealMapper {
             "from setmeal_dish sd left join dish d on sd.dish_id = d.id " +
             "where sd.setmeal_id = #{setmealId}")
     List<DishItemVO> getDishItemBySetmealId(Long setmealId);
+
+    Integer countByMap(Map map);
 }
